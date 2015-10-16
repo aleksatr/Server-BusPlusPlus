8ee75bea07d86a6adbb05a9ef732a760734c8b35
aleksatr <aleksatr@gmail.com>
2015-10-17 00:04:42 +0200

    hi!

--- src/datalayer/CSInfo.java
+++ src/datalayer/CSInfo.java
@@ -6,12 +6,35 @@ public class CSInfo
 	public double lon;
 	public int crowded;
 	public int stuffy;
+	public String brojLinije;
+	public String smerLinije;
+	public Integer stanica = null;			//poslednja stanica za crowd sensing
+	public Integer udaljenost = null;		//udaljenost od poslednje stanice za crowd sensing
+	public String message;
 	
-	public CSInfo(double lat, double lon, int crowded, int stuffy)
+	public CSInfo(double lat, double lon, int crowded, int stuffy, 
+			String brojLinije, String smerLinije, String message)
 	{
 		this.lat = lat;
 		this.lon = lon;
 		this.crowded = crowded;
 		this.stuffy = stuffy;
+		this.brojLinije = brojLinije;
+		this.smerLinije = smerLinije;
+		this.message = message;
+	}
+	
+	public void usrednji(CSInfo csInfo)
+	{
+		this.crowded = (this.crowded + csInfo.crowded) / 2;
+		this.stuffy = (this.stuffy + csInfo.stuffy) / 2;
+		
+		this.message += "+busSEPARATOR+" + csInfo.message;
+		
+		//od koordinata pamti samo najnovije
+		this.lat = csInfo.lat;
+		this.lon = csInfo.lon;
+		this.stanica = csInfo.stanica;
+		this.udaljenost = csInfo.udaljenost;
 	}
 }
